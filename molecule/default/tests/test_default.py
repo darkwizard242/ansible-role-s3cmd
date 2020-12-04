e0af88f9b1f739fba1b410e0e81f087732279cf8
darkwizard242 <am900820@gmail.com>
2020-12-04 10:07:51 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 's3cmd'
+PACKAGE_BINARY = '/usr/local/bin/s3cmd'
+
+
 def test_s3cmd_binary_exists(host):
-    assert host.file('/usr/local/bin/s3cmd').exists
+    """
+    Tests if s3cmd binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_s3cmd_binary_file(host):
-    assert host.file('/usr/local/bin/s3cmd').is_file
+    """
+    Tests if s3cmd binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_s3cmd_binary_which(host):
-    assert host.check_output('which s3cmd') == '/usr/local/bin/s3cmd'
+    """
+    Tests the output to confirm s3cmd's binary location.
+    """
+    assert host.check_output('which s3cmd') == PACKAGE_BINARY
